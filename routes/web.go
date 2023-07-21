3c86545a67d7eb8c1c395b9782a70fb739b332e3
kkumar-gcc <krishkumar9352@gmail.com>
2023-07-21 21:01:13 +0530

    Initial commit

---
+++ routes/web.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"github.com/goravel/framework/contracts/http"
+	"github.com/goravel/framework/facades"
+
+	"goravel/app/http/controllers"
+)
+
+func Web() {
+	facades.Route().Get("/", func(ctx http.Context) {
+		ctx.Response().Json(http.StatusOK, http.Json{
+			"Hello": "Goravel",
+		})
+	})
+
+	userController := controllers.NewUserController()
+	facades.Route().Get("/users/{id}", userController.Show)
+}
